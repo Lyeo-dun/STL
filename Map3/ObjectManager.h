bfd9b5c48a588808d8293b2a307d718d2ba16c1b
hjd0830 <hjd0830@gmail.com>
2021-07-27 13:55:50 +0900

    Edit 2

--- Map3/ObjectManager.h
+++ Map3/ObjectManager.h
@@ -18,6 +18,15 @@ private:
 
 public:
 	void AddObject(Object* _pObj);
+	list<Object*>* GetList(string _strKey) {
+		map<string, list<Object*>>::iterator iter = ObjectList.find("_strKey");
+
+		if (iter == ObjectList.end())
+			return NULL;
+
+		return &iter->second;
+	}
+
 	void Update();
 	void Render();
 	void Release();
